34a1a9175a172619b0f44cc7ace758a641be2f14
LindaHaviv <linda.haviv@gmail.com>
2016-02-26 13:15:14 -0500

    gemfile fix 3

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-config.gem "httparty"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
@@ -76,4 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  
+  config.gem "httparty"
 end
