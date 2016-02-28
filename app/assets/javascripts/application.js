7f788914fce7aae0dc1d3774c4e8097784dc5bae
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-28 03:12:43 -0500

    instruction bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,26 @@
 //= require d3
 //= require nv.d3.min
 //= require_tree .
+
+
+$(document).ready(function () {
+  //can also be wrapped with:
+  //1. $(function () {...});
+  //2. $(window).load(function () {...});
+  //3. Or your own custom named function block.
+  //It's better to wrap it.
+
+  //Tooltip, activated by hover event
+  $("body").tooltip({   
+    selector: "[data-toggle='tooltip']",
+    container: "body"
+  })
+    //Popover, activated by clicking
+    .popover({
+    selector: "[data-toggle='popover']",
+    container: "body",
+    html: true
+  });
+  //They can be chained like the example above (when using the same selector).
+  
+});
